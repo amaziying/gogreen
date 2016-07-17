7edd6bd3f2772067cabfa1262cbe42fedc6e6221
Felix Wei <felixwei@hotmail.ca>
2016-07-17 16:59:15 -0400

    community page

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularRoute from 'angular-route';
 import uiBootstrap from 'angular-ui-bootstrap';
+import smartTable from 'angular-smart-table';
 import config from './config';
 import homeController from './controllers/homeController';
 import historyController from './controllers/historyController';
@@ -13,7 +14,7 @@ import 'jquery';
 
 var moduleName = "app";
 var app = angular.module(moduleName, [
- 'ngRoute', 'ui.bootstrap'
+ 'ngRoute', 'ui.bootstrap', 'smart-table'
 ]).config(config)
   .controller('historyController', historyController)
   .controller('mainController', ['$scope', '$http', '$timeout','$location', mainController])
--- static/js/controllers/communityController.js
+++ static/js/controllers/communityController.js
@@ -1,6 +1,29 @@
 function communityController($scope, $http){
+	$scope.userCollection = [
+		{rank: 1, name: "userOne", score: 500, level: 3},
+		{rank: 1, name: "userTwo", score: 400, level: 4},
+		{rank: 1, name: "userThree", score: 300, level: 5},
+		{rank: 1, name: "userFowur", score: 61, level: 6},
+		{rank: 1, name: "userFoeur", score: 60, level: 6},
+		{rank: 1, name: "userFofur", score: 6300, level: 6},
+		{rank: 1, name: "userFoeur", score: 6550, level: 6},
+		{rank: 1, name: "userFhour", score: 6003, level: 6},
+		{rank: 1, name: "userFohur", score: 6002, level: 6},
+		{rank: 1, name: "userFohur", score: 6005, level: 6}
+	];
 
-	console.log("This is the Community!");
+	$scope.dataCollection = [
+		{rank: 1, name: "userOne", score: 500, level: 3, trees: 3},
+		{rank: 1, name: "userTwo", score: 400, level: 4},
+		{rank: 1, name: "userThree", score: 300, level: 5},
+		{rank: 1, name: "userFowur", score: 61, level: 6},
+		{rank: 1, name: "userFoeur", score: 60, level: 6},
+		{rank: 1, name: "userFofur", score: 6300, level: 6},
+		{rank: 1, name: "userFoeur", score: 6550, level: 6},
+		{rank: 1, name: "userFhour", score: 6003, level: 6},
+		{rank: 1, name: "userFohur", score: 6002, level: 6},
+		{rank: 1, name: "userFohur", score: 6005, level: 6}
+	];
 };
 
 export default communityController;
