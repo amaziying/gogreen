c23db3c21fe60b92f6d317364a6e841d810426e4
bainian12345 <bainian12345@hotmail.com>
2016-07-16 20:17:13 -0400

    garden page left side
    only ui, no js yet

--- static/js/controllers/plantController.js
+++ static/js/controllers/plantController.js
@@ -1,5 +1,22 @@
 function plantController($scope, $http){
 	$scope.data = "data";
+	$scope.seeds = [
+		{
+			"imgName:": "Oak_seed",
+		 	"amount": 1,
+		 	"name": "Oak Tree"
+		},
+		{
+			"imgName:": "Evergreen_seed",
+		 	"amount": 1,
+		 	"name": "Evergreen Tree"
+		},
+		{
+			"imgName:": "Maple_seed",
+		 	"amount": 1,
+		 	"name": "Maple Tree"
+		}
+	];
 	console.log("Plant Trees Here!");
 };
 
