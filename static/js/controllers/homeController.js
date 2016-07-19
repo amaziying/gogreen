9c3dd6d4ba25d816f34397dfd7c370a7ee42c631
Ziying Zhang <ziyingzhanginc@gmail.com>
2016-07-19 17:46:57 -0400

    Fix style and enable ipad full screen

--- static/js/controllers/homeController.js
+++ static/js/controllers/homeController.js
@@ -1,40 +1,35 @@
 function homeController($scope, $http, $timeout, scoringService){
-	//initialize homecontroller
-	$scope.showScoreUpdate = false;
-	$scope.levels = {
-		low: {
-			className: 'green',
-			message: 'Awesome job! Keep your garbage disposal at this level to get maximum points!'
-		},
-		med: {
-			className: 'orange',
-			message: 'Looks like there is a lot of garbage in the can! You will be rewarded with less points as the can becomes more full!'
-		},
-		high: {
-			className: 'red',
-			message: 'Oh no, too much garbage in the can! To earn maximum points, throw out less waste next time!'
-		}
-	};
+    //initialize homecontroller
+    $scope.showScoreUpdate = false;
+    $scope.levels = {
+        low: {
+            className: 'green',
+            message: 'Awesome job! Keep your garbage disposal at this level to get maximum points!'
+        },
+        med: {
+            className: 'orange',
+            message: 'Looks like there is a lot of garbage in the can! You will be rewarded with less points as the can becomes more full!'
+        },
+        high: {
+            className: 'red',
+            message: 'Oh no, too much garbage in the can! To earn maximum points, throw out less waste next time!'
+        }
+    };
 
-	function updateScore(newScore) {
-		$scope.score = newScore;
-		$scope.showScoreUpdate = true;
-		$timeout(function() {
-			$scope.showScoreUpdate = false;
-		}, 1000);
-	}
+    function updateScore(newScore) {
+        $scope.score = newScore;
+        $scope.showScoreUpdate = true;
+        $timeout(function() {
+            $scope.showScoreUpdate = false;
+        }, 1000);
+    }
 
+    scoringService.subscribe(updateScore);
 
-	function updateWeightLevel(low) {
-
-	}
-
-	scoringService.subscribe(updateScore);
-
-	$scope.statusLevel = 'med';
-	$scope.countdown = '00:10:45';
-	$scope.pointIncrementLevel = 50;
-	$scope.treesPlanted = 20;
+    $scope.statusLevel = 'med';
+    $scope.countdown = '00:10:45';
+    $scope.pointIncrementLevel = 50;
+    $scope.treesPlanted = 20;
 };
 
 export default homeController;
